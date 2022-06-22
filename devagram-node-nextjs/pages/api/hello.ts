64ac1c79a5c27892a8123eb2558ca2076b707480
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-22 15:25:24 -0300

    Limpando projeto para ficar apenas o backend e iniciando a rota de login para teste

--- devagram-node-nextjs/pages/api/hello.ts
+++ devagram-node-nextjs/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+  idade: number;
+  profissao: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res
+    .status(200)
+    .json({
+      name: "Diego Ferreira",
+      idade: 35,
+      profissao: "Desenvolvedor Fullstack"
+    });
 }
