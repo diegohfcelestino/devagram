273227d0df9afbddce031ccf76d30c85e61b0804
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-22 18:40:46 -0300

    Criando api de cadastro, validando cadastro com o banco de dados, criptografando com md5, validando usuarios com mesmo email

---
+++ devagram-node-nextjs/pages/api/cadastro.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import type { CadastroRequisicao } from "../../types/CadastroRequisicao";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import md5 from "md5";
+import { conectarMongoDb } from "../../middlewares/conectarMongoDB";
+
+const endpointCadastro = async (
+  req: NextApiRequest,
+  res: NextApiResponse<RespostaPadraoMsg>
+) => {
+  if (req.method === "POST") {
+    const usuario = req.body as CadastroRequisicao;
+
+    if (!usuario.nome || usuario.nome.length < 2) {
+      return res.status(400).json({ erro: "Nome inválido" });
+    }
+
+    if (
+      !usuario.email ||
+      usuario.email.length < 5 ||
+      !usuario.email.includes("@") ||
+      !usuario.email.includes(".")
+    ) {
+      return res.status(400).json({ erro: "Email inválido" });
+    }
+
+    if (!usuario.senha || usuario.senha.length < 4) {
+      return res.status(400).json({ erro: "Senha inválida" });
+    }
+
+    //validar se ja existe usuario com o mesmo email
+    const usuariosComMesmoEmail = await UsuarioModel.find({
+      email: usuario.email
+    });
+    if (usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0) {
+      return res
+        .status(400)
+        .json({ erro: " Já existe uma conta com o email informado" });
+    }
+
+    //Salvar no banco de dados, criptografando com o md5
+    const usuarioASerSalvo = {
+      nome: usuario.nome,
+      email: usuario.email,
+      senha: md5(usuario.senha)
+    };
+    await UsuarioModel.create(usuarioASerSalvo);
+    return res.status(200).json({ msg: "Usuário criado com sucesso" });
+  }
+  return res.status(405).json({ erro: "Método informado não é válido" });
+};
+
+export default conectarMongoDb(endpointCadastro);
--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,16 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDb } from "../../middlewares/conectarMongoDB";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import md5 from "md5";
+import { UsuarioModel } from "../../models/UsuarioModel";
 
-const endpointLogin = (
+const endpointLogin = async (
   req: NextApiRequest,
   res: NextApiResponse<RespostaPadraoMsg>
 ) => {
   if (req.method === "POST") {
     const { login, senha } = req.body;
 
-    if (login === "admin@admin.com" && senha === "Admin@123") {
-      return res.status(200).json({ msg: "Usuário autenticado com sucesso" });
+    const usuariosEncontrados = await UsuarioModel.find({
+      email: login,
+      senha: md5(senha)
+    });
+    if (usuariosEncontrados && usuariosEncontrados.length > 0) {
+      const usuarioEncontrado = usuariosEncontrados[0];
+      return res
+        .status(200)
+        .json({
+          msg: `Usuário ${usuarioEncontrado.nome} autenticado com sucesso`
+        });
     }
     return res.status(400).json({ erro: "Usuário ou senha inválidos" });
   }
