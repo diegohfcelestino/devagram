b18018dd0782d2bbc9855411a5c98ca3b49a42ef
diegohfcelestino <diegohfcelestino@gmail.com>
2022-06-30 18:28:01 -0300

    Criando tela de login para dispositivo mobile e desktop

---
+++ devagram-react-nextjs/components/login/index.js
@@ -0,0 +1,57 @@
+import InputPublico from "../inputPublico";
+import Image from "next/image";
+import Botao from "../botao";
+
+import imagemEnvelope from '../../public/imagens/envelope.svg';
+import imagemChave from '../../public/imagens/chave.svg';
+import imagemLogo from '../../public/imagens/logo.svg';
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  return (
+    <section className={`paginaLogin paginaPublica`}>
+      <div className="logoContainer">
+        <Image
+          src={imagemLogo}
+          alt="Logotipo"
+          layout="fill"
+          className="logo"
+        />
+      </div>
+
+      <div className="conteudoPaginaPublica">
+        <form action="">
+          <InputPublico
+            imagem={imagemEnvelope}
+            texto="E-mail"
+            tipo="email"
+            aoAlterarValor={e => setEmail(e.target.value)}
+            valor={email}
+          />
+
+          <InputPublico
+            imagem={imagemChave}
+            texto="Senha"
+            tipo="password"
+            aoAlterarValor={e => setSenha(e.target.value)}
+            valor={senha}
+          />
+
+          <Botao
+            texto="Login"
+            tipo="submit"
+            desabilitado={false}
+          />
+        </form>
+        <div className="rodapePaginaPublica">
+          <p>Não possui uma conta?</p>
+          <Link href="/cadastro">Faça seu cadastro agora</Link>
+        </div>
+      </div>
+    </section>
+  );
+}
--- devagram-react-nextjs/pages/index.js
+++ devagram-react-nextjs/pages/index.js
@@ -1,25 +1,7 @@
-import { useRef, useState } from 'react';
-import Avatar from '../components/avatar';
-import Botao from '../components/botao';
-import UploadImagem from '../components/uploadImagem';
+import Login from "../components/login";
 
-export default function Home() {
-  const [imagem, setImagem] = useState(null);
-  const referenciaInput = useRef(null);
-  console.log(imagem);
+export default function Index() {
   return (
-    <>
-      <h1>Olá mundo!</h1>
-      <button onClick={() => referenciaInput?.current?.click()}>Abrir seletor de arquivos</button>
-      <UploadImagem
-        setImagem={setImagem}
-        imagemPreview={imagem?.preview}
-        aoSetarAReferencia={(ref) => referenciaInput.current = ref}
-      />
-      <div style={{ width: 200 }}>
-        <Avatar />
-        <Botao texto={'Login'} cor="primaria" manipularClique={() => console.log("Botão clicado")} />
-      </div>
-    </>
+    <Login />
   );
 }
