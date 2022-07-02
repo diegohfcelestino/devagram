398476b046e0115de9d48d991fa5f43aed93813e
diegohfcelestino <diegohfcelestino@gmail.com>
2022-07-02 20:20:19 -0300

    Criando componente de resultado de pesquisa de usuario e colocando estilos mobile e desktop

--- devagram-react-nextjs/components/layout/Cabecalho.js
+++ devagram-react-nextjs/components/layout/Cabecalho.js
@@ -1,9 +1,49 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import logoHorizontalImg from '../../public/imagens/logoHorizontal.svg';
 import imagemLupa from '../../public/imagens/lupa.svg';
 import Navegacao from './Navegacao';
+import ResultadoPesquisa from './ResultadoPesquisa';
 
 export default function Cabecalho() {
+  const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+  const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+  const aoPesquisar = (e) => {
+    setTermoPesquisado(e.target.value);
+    setResultadoPesquisa([]);
+
+    if (termoPesquisado.length < 3) {
+      return;
+    }
+    setResultadoPesquisa([
+      {
+        avatar: '',
+        nome: "Diego",
+        email: 'testediego@diego.com',
+        _id: '21324331'
+      },
+      {
+        avatar: '',
+        nome: "Aline",
+        email: 'bbbbb@diego.com',
+        _id: '213245454331'
+      },
+      {
+        avatar: '',
+        nome: "Tiago",
+        email: 'aaaaaa@diego.com',
+        _id: '21324676331'
+      }
+    ]);
+  };
+
+  const aoClicarResultadoPesquisa = (id) => {
+    consolelog("aoClicarResultadoPesquisa", id);
+
+
+  };
+
   return (
     <header className="cabecalhoPrincipal">
       <div className="conteudoCabecalhoPrincipal">
@@ -25,14 +65,29 @@ export default function Cabecalho() {
           <input
             type="text"
             placeholder='Pesquisar'
-            value={''}
-            onChange={() => console.log("Pesquisando")}
+            value={termoPesquisado}
+            onChange={aoPesquisar}
           />
         </div>
 
         <Navegacao className='desktop' />
 
       </div>
+
+      {resultadoPesquisa.length > 0 && (
+        <div className='resultadoPesquisaContainer'>
+          {resultadoPesquisa.map(r => (
+            <ResultadoPesquisa
+              key={r.id}
+              avatar={r.avatar}
+              nome={r.nome}
+              email={r.email}
+              id={r.id}
+              onClick={aoClicarResultadoPesquisa}
+            />
+          ))}
+        </div>
+      )}
     </header>
   );
 }
