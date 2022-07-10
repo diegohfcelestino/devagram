e2715ddebd0a2148bfecd12c59cdbc091b376a7f
diegohfcelestino <diegohfcelestino@gmail.com>
2022-07-10 13:49:50 -0300

    Finalizacao do Feed e iniciando a tela de perfil

--- devagram-react-nextjs/components/cabecalhoComAcoes/index.js
+++ devagram-react-nextjs/components/cabecalhoComAcoes/index.js
@@ -1,8 +1,35 @@
-export default function CabecalhoComAcoes() {
-  className;
+import Image from "next/image";
+
+
+export default function CabecalhoComAcoes({
+  className, iconeEsquerda, textoEsquerda = null, aoClicarAcaoEsquerda, titulo, elementoDireita
+}) {
   return (
     <div className={`cabecalhoComAcoes ${className}`} >
+      {iconeEsquerda ? (
+        <Image
+          src={iconeEsquerda}
+          alt="Ícone seta esquerda com ações"
+          onClick={aoClicarAcaoEsquerda}
+          width={25}
+          height={25}
+        />
+      ) : (
+        textoEsquerda !== null && (
+          <span className="cabecalhoComAcoesTextoEsquerda" onClick={aoClicarAcaoEsquerda}>{textoEsquerda}</span>
+        )
+      )}
 
+      <h3>{titulo}</h3>
+
+      {elementoDireita && (
+        <button
+          type='button'
+        >
+          {elementoDireita}
+        </button>
+
+      )}
     </div>
   );
 }
---
+++ devagram-react-nextjs/components/cabecalhoPerfil/index.js
@@ -0,0 +1,41 @@
+import CabecalhoComAcoes from "../cabecalhoComAcoes";
+import Botao from "../botao";
+import imgSetaEsquerda from '../../public/imagens/setaEsquerda.svg';
+import Avatar from "../avatar";
+
+export default function CabecalhoPerfil({
+  usuario,
+  usuarioLogado,
+}) {
+  return (
+    <div className="cabecalhoPerfil largura30pctDesktop">
+      <CabecalhoComAcoes
+        iconeEsquerda={imgSetaEsquerda}
+        titulo={usuario.nome}
+      />
+      <div className="statusPerfil">
+        <Avatar src={usuario.avatar} />
+        <div className="informacoesPerfil">
+          <div className="statusContainer">
+            <div className="status">
+              <strong>15</strong>
+              <span>Publicações</span>
+            </div>
+            <div className="status">
+              <strong>120</strong>
+              <span>Seguidores</span>
+            </div>
+            <div className="status">
+              <strong>135</strong>
+              <span>Seguindo</span>
+            </div>
+          </div>
+          <Botao
+            texto={"seguir"}
+            cor="primaria"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
