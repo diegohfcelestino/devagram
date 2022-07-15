7b1ed6347e26cff479bbee86c7a6c6624e668c0b
diegohfcelestino <diegohfcelestino@gmail.com>
2022-07-14 22:19:26 -0300

    Adicionando bibliotecas, baixando fonts, colocando os icones/imagens

--- devagram-react-native-expo/App.tsx
+++ devagram-react-native-expo/App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
-  return (
+  const [fontsLoaded] = useFonts({
+    "biennale-bold": require("./assets/fonts/Biennale-Bold.otf"),
+    "biennale-regular": require("./assets/fonts/Biennale-Regular.otf")
+  });
+
+  return fontsLoaded ? (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: "biennale-regular" }}>Olá mundo!</Text>
       <StatusBar style="auto" />
     </View>
+  ) : (
+    <AppLoading />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
